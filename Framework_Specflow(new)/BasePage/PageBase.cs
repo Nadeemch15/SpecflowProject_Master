1a291d66b7803e3e108d870d108f4ae44dce0e2b
Nadeemch15 <43064680+Nadeemch15@users.noreply.github.com>
2020-12-26 23:21:35 +0000

    1st commit

---
+++ Framework_Specflow(new)/BasePage/PageBase.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework_Specflow_new_.BasePage
+{
+    public class PageBase
+    {
+        private IWebDriver driver;
+        public PageBase(IWebDriver _driver)
+        {
+            PageFactory.InitElements(_driver, this);
+        }
+    }
+}
